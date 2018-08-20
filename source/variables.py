221b032b6dbbde11a509d25eaf6245dd3d7358fc
teja007 <saitejakolla95@gmail.com>
2018-08-20 08:07:50 +0530

    version 0.1

---
+++ source/variables.py
@@ -0,0 +1,17 @@
+emp_name = "xyz"
+emp_id = 1234
+emp_email = "xyz@gmail.com"
+emp_salary = 2435.24
+emp_address = "22/a , raja nagar" \
+              "ameerpet, hyderabad"
+emp_phone = 9441007777
+system_ip ="10.1.3.4"
+print(emp_name ,emp_phone ,emp_id)
+emp_name ="abc"
+print(emp_name)
+
+a=10
+b =10
+b=20
+
+print(a ,b)
